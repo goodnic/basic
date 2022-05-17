b1136d3181ec5ec159a9766d731907771449db59
Nicola Good <good.nicola94@gmail.com>
2022-05-17 12:46:12 +0000

    using http instead of http-socket

--- src/backend/run-web.sh
+++ src/backend/run-web.sh
@@ -2,4 +2,4 @@ set -e
 
 test -z $PORT && PORT=80
 
-exec poetry run uwsgi --wsgi-file app.py --http-socket :$PORT --honour-stdin
+exec poetry run uwsgi --wsgi-file app.py --http :$PORT --honour-stdin
