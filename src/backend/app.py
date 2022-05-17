57188e2982760efdca6b16ae5bb7d8e7ddd991a0
Nicola Good <good.nicola94@gmail.com>
2022-05-17 11:34:44 +0000

    adding callable

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/hidden")
-def hello_world():
-    return "Hello World"
-
-if __name__ == "__main__":
-    app.run()
+@staticmethod
+def create_app():
+    app = Flask(__name__)
+    return app
